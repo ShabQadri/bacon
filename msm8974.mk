602c62590613f0beca0cb881e200ac5059e6f54e
Tommaso Fonda <fondatommaso2@gmail.com>
2021-01-25 15:20:50 +0100

    msm8974-common: build the F-Droid privileged extension
    Change-Id: I4cbbb117a45a5021f26518e490148908317ea299

--- msm8974.mk
+++ msm8974.mk
@@ -95,6 +95,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# F-Droid
+PRODUCT_PACKAGES += \
+    F-DroidPrivilegedExtension
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
